a59873fbce62fb0970f2fbb64afb539994c7d6bc
sammedhi <sammedhi2@hotmail.fr>
2017-10-16 19:25:56 +0200

    Directory cleaned + segmentation experiment (success)

--- src/image_butcher.py
+++ src/image_butcher.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
-
 import imageio
+from utils import *
 
 def PILtoNpArray(image):
 	return np.array(image.getdata(),np.uint8).reshape(image.size[1],image.size[0],3)
@@ -9,7 +9,7 @@ def PILtoNpArray(image):
 class ImageButcher:
 	def __init__(self,size_factor):
 		self.IMG_SIZE = (720,500) 
-		self.MIN_SIZE = 60
+		self.MIN_SIZE = 100
 		self.size_factor = size_factor
 		
 	def get_batches(self,image):
@@ -32,7 +32,7 @@ class ImageButcher:
 		step_x = int(windowSize[0]/2)
 		for y in range(0, image.shape[0] - step_y , step_y):
 			raw = []
-			for x in range(0, image.shape[1] - step_x ,step_x):
+			for x in range(0, image.shape[1] - step_x,step_x):
 				# yield the current window
 				if(x + windowSize[0] > self.IMG_SIZE[0] or y + windowSize[1] > self.IMG_SIZE[1]):
 					break
@@ -41,9 +41,16 @@ class ImageButcher:
 				batch.append(raw)
 		return batch
 
+	def get_metadata(self,x,y,z):
+		size_batch = self.IMG_SIZE[0] / self.size_factor**z , self.IMG_SIZE[1] / self.size_factor**z
+		step_x = size_batch[0] / 2.0
+		step_y = size_batch[1] / 2.0
+		return	(size_batch,step_x * x,step_y * y)
 
 if __name__ == "__main__":
-	img = imageio.imread("bird.jpg")
+	from utils import *
+	from PIL import ImageDraw
+	img = imageio.imread("bigbird.jpg")
 	batcher = ImageButcher(2.0)
 	batches = batcher.get_batches(img)
 
@@ -58,4 +65,25 @@ if __name__ == "__main__":
 			else:
 				all_list = np.concatenate((all_list,xlist),axis=0)
 
-		Image.fromarray(all_list).show()
+		#Image.fromarray(all_list).show()
+	model = load_inception()
+	pred = batch_label_matching(model,batches)
+	image = Image.fromarray(img)
+	image = image.resize((720,500))
+	drw = ImageDraw.Draw(image)
+	for z in range(1,2):
+		for y in range(len(pred[z])):
+			for x in range(len(pred[z][0])):
+				#print("{0} for {1}".format(pred[z][y][x],batcher.get_metadata(x,y,z)))
+				metadata = batcher.get_metadata(x,y,z)
+				pos = int(metadata[1]) , int(metadata[2])
+				pos2 = int(pos[0] + metadata[0][0]) , int(pos[1] + metadata[0][1])
+				print(pos)
+				print(pos2)
+				if(len(pred[z][y][x]) == 1 and "Bee Eater" in pred[z][y][x]):
+					drw.rectangle((pos , pos2),outline=(0,255,0))
+
+	image.show()
+
+
+
