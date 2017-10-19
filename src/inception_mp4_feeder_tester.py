6dca7c73db066d2f85805e44054f44eb682e23c2
Yliess HATI <hatiyliess86@gmail.com>
2017-10-19 13:10:44 +0200

    Log tensorboard + Fix

--- src/inception_mp4_feeder_tester.py
+++ src/inception_mp4_feeder_tester.py
@@ -2,27 +2,28 @@ from mp4_slicer import *
 from image_butcher import *
 from utils import batch_label_matching
 
-import argparser
-import tqdm
+import argparse
+
+from PIL import ImageDraw
+from tqdm import tqdm
 
 
-if __name__ == "main":
+if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Feed the inception model with a given mp4 and perform detection over the stream")
 	parser.add_argument("-f","--file",help="The mp4 file")
-	parser.add_argument("-t","--timestamp",default=1,help="The elapsed time between each detection update",type=float)
+	parser.add_argument("-t","--timestamp",default=1,help="The elapsed time between each detection update",type=int)
 	parser.add_argument("-r","--imgRate",default=30,help="The image rate of the given mp4",type=int)
-	parser.add_argument("-f","--factor",default="1.5",help="Size factor by which the patch is reduce at each iteration",type=float)
+	parser.add_argument("-a","--alpha",default="1.5",help="Size factor by which the patch is reduce at each iteration",type=float)
 	parser.add_argument("-pf","--patch_factor",default="1:4",help="Reduction factor boundaries")
 
 	args = parser.parse_args()
 	args.patch_factor = int(args.patch_factor.split(':')[0]) , int(args.patch_factor.split(':')[1])
 
 	imgs = slice_mp4(args.file,args.timestamp,args.imgRate)
-	image_butcher = ImageButcher(args.factor,args.patch_factor)
+	image_butcher = ImageButcher(args.alpha,args.patch_factor)
 
 	for img in tqdm(imgs):
 		batches = image_butcher.get_batches(img)
-		probability.append(batch_label_matching(batches))
 		for batch in batches:
 			all_list = None 
 			for y in batch:
@@ -42,10 +43,10 @@ if __name__ == "main":
 			for y in range(len(pred[z])):
 				for x in range(len(pred[z][0])):
 					#print("{0} for {1}".format(pred[z][y][x],batcher.get_metadata(x,y,z)))
-					metadata = batcher.get_metadata(img,x,y,z)
+					metadata = image_butcher.get_metadata(img,x,y,z)
 					pos = int(metadata[1]) , int(metadata[2])
 					pos2 = int(pos[0] + metadata[0][0]) , int(pos[1] + metadata[0][1])
-					if(len(pred[z][y][x]) == 1 and "Bee Eater" in pred[z][y][x]):
+					if(len(pred[z][y][x]) == 1 and "Canadian Goose" in pred[z][y][x]):
 						print(pos)
 						print(pos2)
 						drw.rectangle((pos , pos2),outline=(0,255,0))
