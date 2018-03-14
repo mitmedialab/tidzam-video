6f1a1e39c3501b430a0768b4a6bcbd9828053c73
slash6475 <duhart@mit.edu>
2018-03-14 12:45:39 -0400

    Define a parameter in cfg file for the activation of frame boxing

--- src/supervisor/jobs/boxer/boxerjob.py
+++ src/supervisor/jobs/boxer/boxerjob.py
@@ -10,27 +10,32 @@ def encode_results(results):
 	r = []
 	for elem in results:
 		r.append( (elem[0].decode(encoding="utf-8"), elem[1], elem[2]) )
-		
+
 	return r
 
 class Boxerjob(Job):
 	def setup(self, data):
+		self.debug_boxing = False
+		if type(data) == type({}):
+			self.debug_boxing = data.get("debug_boxing")
+
 		config = b"cfg/yolo9000.cfg"
 		weights = b"weights/yolo9000.weights"
-		meta = b"cfg/combine9k.data"		
+		meta = b"cfg/combine9k.data"
 		self.detector = Detector(config, weights, meta)
 		debug("Started detector cfg="+str(config)+" weights="+str(weights)+" meta="+str(meta), 3)
-		
-		
+
+
 	def loop(self, data):
 		image = data.img
 
 		results = self.detector.run(image)
 		image = self.detector.draw_boxes(results, image)
-		
-		data['img'] = image # COMMENT OUT TO UNDO BOXES DRAWING
+
+		if self.debug_boxing is True:
+			data['img'] = image # COMMENT OUT TO UNDO BOXES DRAWING
 		data['detection'] = encode_results(results)
-		
+
 		return data
 
 	def requireData(self):
